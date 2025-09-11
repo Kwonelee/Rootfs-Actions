45a2393215f4efced87f07326da2a504dd669a0a
Kwonelee <995546054@qq.com>
2025-09-11 20:48:18 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,12 +7,9 @@
 # Blog: https://p3terx.com
 #===============================================
 
-# 集成首次运行脚本
-cp $GITHUB_WORKSPACE/configfiles/99-custom.sh package/base-files/files/etc/uci-defaults/
-
 # 添加定时限速
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 
 # 添加amlogic
-#rm -rf package/luci-app-amlogic
-#git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
