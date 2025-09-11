cbe573b0c0d4d02778e5edd3a7fc274fe2b6de04
Kwonelee <995546054@qq.com>
2025-09-11 17:03:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,6 +7,9 @@
 # Blog: https://p3terx.com
 #===============================================
 
+# 集成开机脚本
+cp $GITHUB_WORKSPACE/configfiles/99-custom.sh package/base-files/files/etc/uci-defaults/
+
 # 定时限速插件
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 
