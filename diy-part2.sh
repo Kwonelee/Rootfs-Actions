83caa9e6069bbb91ac6927402cf0a154068e0b58
Kwonelee <995546054@qq.com>
2025-09-13 23:07:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,26 @@
 rm -rf package/luci-app-amlogic
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+# 添加turboacc
+rm -rf feeds/luci/applications/luci-app-turboacc
+git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
 
-# 添加openlist2
-#git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 package/openlist
+#在线OTA
+rm -rf package/diy/luci-app-ota
+git_sparse_clone main https://github.com/sos801107/istoreos-ota luci-app-ota
+git_sparse_clone main https://github.com/sos801107/istoreos-ota fw_download_tool
+
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# golong25.x依赖
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+
+# 必要的补丁
+pushd
+   curl -sSL https://raw.githubusercontent.com/Jaykwok2999/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+popd
+
+
+./scripts/feeds update -a
+./scripts/feeds install -a
