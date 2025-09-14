3b66a557cac18f26b4824154c7ac1fb9070dcfb7
Kwonelee <995546054@qq.com>
2025-09-14 16:31:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -49,6 +49,7 @@ function merge_package() {
 }
 
 git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
+git_sparse_clone packages https://github.com/chenmozhijin/turboacc
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 git_sparse_clone main https://github.com/kiddin9/luci-app-adguardhome
 git_sparse_clone main https://github.com/Kwonelee/istoreos-ota luci-app-ota
