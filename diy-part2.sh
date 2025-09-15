1f1bf2e8bb348072a7b8ce88471c383754e4497e
Kwonelee <995546054@qq.com>
2025-09-15 20:33:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,70 +9,7 @@
 
 # 移除待替换/非必要包
 rm -rf feeds/third_party/luci-app-LingTiGameAcc
-rm -rf feeds/packages/net/filebrowser
-rm -rf feeds/kenzo/luci-app-filebrowser
-rm -rf feeds/kenzo/luci-app-adguardhome
-rm -rf feeds/luci/applications/luci-app-turboacc
-rm -rf package/diy/luci-app-ota
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
-function merge_package() {
-    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
-    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-    if [[ $# -lt 3 ]]; then
-        echo "Syntax error: [$#] [$*]" >&2
-        return 1
-    fi
-    trap 'rm -rf "$tmpdir"' EXIT
-    branch="$1" curl="$2" target_dir="$3" && shift 3
-    rootdir="$PWD"
-    localdir="$target_dir"
-    [ -d "$localdir" ] || mkdir -p "$localdir"
-    tmpdir="$(mktemp -d)" || exit 1
-    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
-    cd "$tmpdir"
-    git sparse-checkout init --cone
-    git sparse-checkout set "$@"
-    # 使用循环逐个移动文件夹
-    for folder in "$@"; do
-        mv -f "$folder" "$rootdir/$localdir"
-    done
-    cd "$rootdir"
-}
-
-git_sparse_clone main https://github.com/kenzok8/small-package filebrowser
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-filebrowser
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-ramfree
-git_sparse_clone package https://github.com/chenmozhijin/turboacc
-git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
-#git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-git_sparse_clone js https://github.com/sirpdboy/luci-app-adguardhome luci-app-adguardhome
-#git_sparse_clone main https://github.com/kiddin9/luci-app-adguardhome
-git_sparse_clone main https://github.com/Kwonelee/istoreos-ota luci-app-ota
-git_sparse_clone main https://github.com/Kwonelee/istoreos-ota fw_download_tool
 
 # 添加luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-
-# golong25.x依赖
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
-
-# 必要的补丁
-pushd
-   curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-popd
-
-
-./scripts/feeds update -a
-./scripts/feeds install -a
